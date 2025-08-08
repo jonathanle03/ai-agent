72655e97fbfd60e0de767c1f74c7f3242f5d52d2
jonathanle03 <jonathanle062303@gmail.com>
2025-08-08 00:39:19 -0700

    Take prompt from command line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,7 +7,11 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+if len(sys.argv) == 1:
+    print("Invalid usage: uv main.py <prompt>")
+    sys.exit(1)
+
+prompt = sys.argv[1]
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 
 print(response.text)
