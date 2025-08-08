f6d7deb967f68de4a097932ebd934e54284acfa3
jonathanle03 <jonathanle062303@gmail.com>
2025-08-08 01:03:53 -0700

    Create list of responses

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 if len(sys.argv) == 1:
     print("Invalid usage: uv main.py <prompt>")
     sys.exit(1)
 
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
 prompt = sys.argv[1]
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)])
+]
+
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages
+)
+
 
 print(response.text)
 print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
