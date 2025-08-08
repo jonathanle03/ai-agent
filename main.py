09f9412676fa5bab784c2f10b672aa6114a66c09
jonathanle03 <jonathanle062303@gmail.com>
2025-08-08 03:46:04 -0700

    Add verbose option

--- main.py
+++ main.py
@@ -5,28 +5,47 @@ from google import genai
 from google.genai import types
 
 
-if len(sys.argv) == 1:
-    print("Invalid usage: uv main.py <prompt>")
-    sys.exit(1)
+def main():
+    prompt = None
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            prompt = arg
+    
+
+    if not prompt:
+        print("Invalid usage: uv main.py <prompt>")
+        sys.exit(1)
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
 
-prompt = sys.argv[1]
-messages = [
-    types.Content(role="user", parts=[types.Part(text=prompt)])
-]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
 
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages
-)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
 
-print(response.text)
-print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+    if "--verbose" in sys.argv:
+        print("User prompt: " + prompt)
+
+
+    print(response.text)
+
+
+    if "--verbose" in sys.argv:
+        print("Prompt tokens: " + str(prompt_tokens))
+        print("Response tokens: " + str(response_tokens))
+
+
+if __name__ == "__main__":
+    main()
