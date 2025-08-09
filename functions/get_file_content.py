f2a8bff216cd5405a76f9076f1f0b7203b8c7ab2
jonathanle03 <jonathanle062303@gmail.com>
2025-08-09 00:54:27 -0700

    Create get_file_content.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+
+
+def get_file_content(working_directory, file_path):
+    actual_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+
+    if not actual_file_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+
+    if not os.path.isfile(actual_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    
+    try:
+        MAX_CHARS = 10000
+        with open(actual_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+
+            if len(file_content_string) == MAX_CHARS:
+                file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+        
+        return file_content_string
+    
+    except Exception as e:
+        return f"Error getting file contents: {e}"
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,10 +2,10 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    actual_directory = os.path.join(working_directory, directory)
+    actual_directory = os.path.abspath(os.path.join(working_directory, directory))
 
 
-    if not os.path.abspath(actual_directory).startswith(os.path.abspath(working_directory)):
+    if not actual_directory.startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
 
