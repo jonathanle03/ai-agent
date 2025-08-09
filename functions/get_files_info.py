261c36c6e8a3e64f2f5ad233f8e9118d0a3d24f2
jonathanle03 <jonathanle062303@gmail.com>
2025-08-09 00:28:55 -0700

    Create get_files_info.py

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    actual_directory = os.path.join(working_directory, directory)
+
+
+    if not os.path.abspath(actual_directory).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+
+    if not os.path.isdir(actual_directory):
+        f'Error: "{directory}" is not a directory'
+    
+
+    try:
+        files = os.listdir(actual_directory)
+        contents = ""
+
+        for file in files:
+            actual_file = os.path.join(actual_directory, file)
+            contents += f" - {os.path.basename(actual_file)}: file_size={os.path.getsize(actual_file)}, is_dir={os.path.isdir(actual_file)}\n"
+        
+        return contents
+    
+    except Exception as e:
+        return f"Error listing files: {e}"
